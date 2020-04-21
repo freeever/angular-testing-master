c93757594bff59345abd44c865582f06255aaf57
Daniel Xu <zxuz111@gmail.com>
2020-04-20 21:42:33 -0400

    Initial by copying over the following course code: $ git clone https://github.com/angular-university/angular-testing-course.git $ git checkout -b 1-start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./courses/home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {CourseComponent} from "./courses/course/course.component";
+import {CourseResolver} from "./courses/services/course.resolver";
 
+const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
 
-const routes: Routes = [];
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: 'courses/:id',
+        component: CourseComponent,
+        resolve: {
+            course: CourseResolver
+        }
+    },
+    {
+        path: "**",
+        redirectTo: '/'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
